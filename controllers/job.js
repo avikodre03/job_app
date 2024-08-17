4e458dfc1e61227f182f0e5df2164cb44ff40037
avikodre03 <akodre111@gmail.com>
2024-08-17 17:14:05 +0530

    DB connection string updated

--- controllers/job.js
+++ controllers/job.js
@@ -1,61 +1,96 @@
 const jobModel = require("../models/job")
 
 const createJob = async (req, res) => {
-    const jobObj = req.body;
-    const newJob = new jobModel(jobObj);
-    const newSaveJobs = await newJob.save();
-    console.log(newJob);
 
-    res.json({
-        success: true,
-        message: "Job created successfully",
-        jobId: newSaveJobs._id
-    })
+    try {
+        const jobObj = req.body;
+        const newJob = new jobModel(jobObj);
+        const newSaveJobs = await newJob.save();
+        console.log(newJob);
+
+        res.json({
+            success: true,
+            message: "Job created successfully",
+            jobId: newSaveJobs._id
+        })
+    } catch (error) {
+        res.json({
+            success: false,
+            message: "something went wrong, please try again after sometime"
+        })
+    }
+
 }
 
 const listJob = async (req, res) => {
-    const minSalary = req.query.minSalary
-    const maxSalary = req.query.maxSalary
-    const jobList = await jobModel.find({
-        $and: [
-            { salary: { $gte: minSalary } },
-            { salary: { $lte: maxSalary } }
-        ]
+    try {
+        const minSalary = req.query.minSalary
+        const maxSalary = req.query.maxSalary
+        const jobList = await jobModel.find({
+            $and: [
+                { salary: { $gte: minSalary } },
+                { salary: { $lte: maxSalary } }
+            ]
 
-    });
-    console.log(jobList);
+        });
+        console.log(jobList);
 
-    res.json({
-        success: true,
-        message: "listJob job",
-        results: jobList,
-    })
+        res.json({
+            success: true,
+            message: "listJob job",
+            results: jobList,
+        })
+    } catch (error) {
+        res.json({
+            success: false,
+            message: "something went wrong, please try again after sometime"
+        })
+    }
+
+
 }
 
 const editjob = async (req, res) => {
-    const jobId = req.params.id;
-    console.log(jobId);
-    console.log(req.body);
+    try {
+        const jobId = req.params.id;
+        console.log(jobId);
+        console.log(req.body);
 
-    // Model.findByIdAndUpdate(_Id to find record,fields data to update)
-    const updatedata = await jobModel.findByIdAndUpdate(jobId, req.body)
-    console.log(updatedata);
+        // Model.findByIdAndUpdate(_Id to find record,fields data to update)
+        const updatedata = await jobModel.findByIdAndUpdate(jobId, req.body)
+        console.log(updatedata);
 
-    res.json({
-        success: true,
-        message: "job updated succefully"
-    })
+        res.json({
+            success: true,
+            message: "job updated succefully"
+        })
+    } catch (error) {
+        res.json({
+            success: false,
+            message: "something went wrong, please try again after sometime"
+        })
+    }
+
 }
 
 const deleteJob = async (req, res) => {
-    const jobId = req.params.id;
-    await jobModel.findByIdAndDelete(jobId)
-// jobModel.findOneAndDelete()
-// jobModel.deleteMany()
-    res.json({
-        success: true,
-        message: "delete job"
-    })
+    try {
+        const jobId = req.params.id;
+        await jobModel.findByIdAndDelete(jobId)
+        // jobModel.findOneAndDelete()
+        // jobModel.deleteMany()
+        res.json({
+            success: true,
+            message: "delete job"
+        })
+    } catch (error) {
+        res.json({
+            success: false,
+            message: "something went wrong, please try again after sometime"
+        })
+    }
+
+
 }
 
 const jobController = {
--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const app = express()
 
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/jobapp")
-    // mongoose.connect("mongodb://localhost:27017/jobapp")
+// mongoose.connect("mongodb://localhost:27017/jobapp")
+// mongoose.connect("mongodb://127.0.0.1:27017/jobapp")
+mongoose.connect("mongodb+srv://akodre111:ZAWGU09em5G74P0q@jobapp.4axhi.mongodb.net/")
     .then(() => {
         console.log("connect db");
     })
